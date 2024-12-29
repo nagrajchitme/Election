05ecc0729c875feb1d5ce79ffc5e7956dc0a4ef5
Nagraj chitme <nagrajvchitme@gmail.com>
2024-12-29 12:49:05 +0530

    election pomplet

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="main">
+        <h1 id="para">
+          Prathamika Krishi Pattina Sangha Ni. Nagmarpalli 2024-25 Tq||Aurad
+          Dist||Bidar
+        </h1>
+        <p>
+          Matadana Date 28-12-2024 time 9:am t 4:pm onwards Place:Prathamika
+          Krishi Pattina Sangha ni Nagmarpalli 2024-25 TQ||Aurad Dist||Bidar
+        </p>
+        <div id="sim">Samannya Kshetra</div>
+        <div id="one">
+          <div id="first">Candidate Name</div>
+          <div id="second">Simbol</div>
+        </div>
+
+        <div id="box1">
+          <div id="box11">1.Ambresh Gangappa Biradar</div>
+          <div id="box12">
+            <div id="box111"></div>
+            <div id="box112"></div>
+          </div>
+        </div>
+        <div id="box2">
+          <div id="box11">2.Chanmallayya Shivmurti </div>
+          <div id="box12">
+            <div id="box113"></div>
+            <div id="box114"></div>
+          </div>
+        </div>
+        <div id="box3">
+          <div id="box11">3.Bhanudas Venkatrao Nayak</div>
+          <div id="box12">
+            <div id="box115"></div>
+            <div id="box116"></div>
+          </div>
+        </div>
+        <div id="box4">
+          <div id="box11">4.Manohar Baburao Biradar</div>
+          <div id="box12">
+            <div id="box117"></div>
+            <div id="box118"></div>
+          </div>
+        </div>
+        <div id="box5">
+          <div id="box11">5.Sharanbasappa Bhimrao</div>
+          <div id="box12">
+            <div id="box119"></div>
+            <div id="box120"></div>
+          </div>
+        </div>
+        <div id="box6">
+          <div id="box11">6.Sangappa Ghaleppa</div>
+          <div id="box12">
+            <div id="box121"></div>
+            <div id="box122"></div>
+          </div>
+        </div>
+        <div id="box7">
+          <div id="box11">7.Nagagouda Sayagouda</div>
+          <div id="box12">
+            <div id="box123"></div>
+            <div id="box124"></div>
+          </div>
+        </div>
+        <div id="box8">
+          <div id="box11">8.Ramgouda Viregouda</div>
+          <div id="box12">
+            <div id="box125"></div>
+            <div id="box126"></div>
+          </div>
+        </div>
+        <div>
+          <p>
+            Atmiya Raita Bhandhavatre Endu Edu Raitarigagi Raitarigoskara Eruva
+            Sanga. Agiddu Raita Sarvottama Abhivriddhi Madlu Sarvangini
+            Vikasakkagi Kra Sa 1 2 3 4 5 6 7 Chinne Gurutoge Tamma Amullyavada
+            Matavannu Nidi Prachanda Bahumatadinda Gellisabekagi
+            Raitabhandavarali Vinanti.
+          </p>
+          <h2 id="last">"Nimma Mata Raitara Vikasakkagi"</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
